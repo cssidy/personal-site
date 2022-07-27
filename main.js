ec0f61c55d2b43d8353817b27ba2cc0d0aad6ed3
Cassidy Arden <hi@cassidyarden.com>
2022-07-27 00:40:32 -0400

    a days work

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello');
+const imagePlaceholder = document.querySelector('.image-placeholder');
+const descriptionPlaceholder = document.querySelector('.description-placeholder');
+const urlPlaceholder = document.querySelector('.url-placeholder');
+const projectList = document.querySelectorAll('.project');
+
+projectList.forEach(function (project) {
+    project.addEventListener('click', changeProjectData);
+    project.addEventListener('mouseenter', changeProjectData);
+});
+
+function changeProjectData() {
+    // console.log(this.id);
+    imagePlaceholder.style.backgroundImage = `url('./assets/projects/${this.id}.gif')`;
+    descriptionPlaceholder.textContent = this.dataset.description;
+
+    if (this.dataset.url) {
+        urlPlaceholder.href = this.dataset.url;
+        urlPlaceholder.textContent = 'Live URL';
+    } else {
+        urlPlaceholder.textContent = '';
+    }
+}
