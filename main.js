26126984d9db68d39fcbc14b3d78e1da9de64683
Cassidy Arden <hi@cassidyarden.com>
2022-07-27 18:21:19 -0400

    reorganize assets

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const imagePlaceholder = document.querySelector('.image-placeholder');
+const projectType = document.querySelector('.project-type');
 const descriptionPlaceholder = document.querySelector('.description-placeholder');
 const urlPlaceholder = document.querySelector('.url-placeholder');
 const projectList = document.querySelectorAll('.project');
@@ -11,7 +12,9 @@ projectList.forEach(function (project) {
 function changeProjectData() {
     // console.log(this.id);
     imagePlaceholder.style.backgroundImage = `url('./assets/projects/${this.id}.gif')`;
+    projectType.textContent = this.dataset.type;
     descriptionPlaceholder.textContent = this.dataset.description;
+
 
     if (this.dataset.url) {
         urlPlaceholder.href = this.dataset.url;
